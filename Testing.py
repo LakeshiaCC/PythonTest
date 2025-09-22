4f74eb153ec08b04722680834c118b9f0d912e21
LakeshiaCC <cooperl2313@student.faytechcc.edu>
2025-09-21 22:12:57 -0400

    Add files via upload

---
+++ Testing.py
@@ -0,0 +1,7 @@
+ #Lakeshia Cooper
+ #09/21/2025
+ #P1LAB1
+ #Python Assignment
+FirstName = input("Enter your first name: ")
+LastName = input("Enter your last name: ") 
+print("Hello " + FirstName + " " + LastName + "! Welcome to CTI-110")
